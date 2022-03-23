001ca92cc0b0dd11de2afdf715f8985cc041140b
david-lafontant <lafontant.david@gmail.com>
2022-03-23 15:28:42 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
 import App from './App';
-// import elementStore from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-
     <App />
-
   </React.StrictMode>,
   document.getElementById('root'),
 );
