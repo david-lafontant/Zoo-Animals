f352895c3c93ee33cd66b55a154c766b249652e1
david-lafontant <lafontant.david@gmail.com>
2022-03-23 15:26:15 -0600

    Fix bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import App from './App';
-import elementStore from './redux/configureStore';
+// import elementStore from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={elementStore}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+
+    <App />
+
   </React.StrictMode>,
   document.getElementById('root'),
 );
