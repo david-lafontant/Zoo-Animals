be650bf55c950e8b92438d096bf822ee10319233
david-lafontant <lafontant.david@gmail.com>
2022-03-24 12:39:04 -0600

    Update Zoo component

--- src/components/zoo/zoo.js
+++ src/components/zoo/zoo.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
