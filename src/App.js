7cfe77bd50e5e6f34d43e380f20b809c4dbd9964
david-lafontant <lafontant.david@gmail.com>
2022-03-24 12:37:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Zoo from './components/zoo/zoo';
 import AnimalDetail from './components/animalDetail/animalDetail';
 import Header from './components/header/header';
-import elementStore from './redux/configureStore';
 
 function App() {
   return (
-    <Provider store={elementStore}>
-      <BrowserRouter>
-        <Container>
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Zoo />} />
-            <Route path="/Zoo" element={<Zoo />} />
-            <Route path="/AnimalDetail/:id" element={<AnimalDetail />} />
-          </Routes>
-        </Container>
-      </BrowserRouter>
-    </Provider>
+
+    <BrowserRouter>
+      <Container>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Zoo />} />
+          <Route path="/Zoo" element={<Zoo />} />
+          <Route path="/AnimalDetail/:id" element={<AnimalDetail />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+
   );
 }
 
