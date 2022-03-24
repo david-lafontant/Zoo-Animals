48ff0a40c59d4e385b20d6fe484e0d99edd248d7
david-lafontant <lafontant.david@gmail.com>
2022-03-24 12:39:33 -0600

    Create and update animal.test.js

--- src/test/animal.test.js
+++ src/test/animal.test.js
@@ -1,44 +1,21 @@
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import elementStore from '../redux/configureStore';
+import { MemoryRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
 import Animal from '../components/animal/animal';
 
-// ARRANGE
-const animal = [
-  {
-    key: 'animalKey',
-    id: 'animalId',
-    type: 'animalType',
-    name: 'animalName',
-    image: 'animalImage',
-    latin_name: 'animalLatin',
-    active_time: 'animalActive',
-    length_min: 'animalMin',
-    length_max: 'animalMax',
-    weight_min: 'animalWeightMin',
-    weight_max: 'animalWeightMax',
-    lifespan: 'animalLifespan',
-    habitat: 'animalHabitat',
-    diet: 'animalDiet',
-    geo_range: 'animalGeoRange',
-    detail: false,
-    item_detail: false,
-
-  },
-];
-
-describe('Test render the Animal Component', () => {
-  it('Animal section renders a hardcoded object, passed as a prop', () => {
-    // ACT
-    const { container } = render(
-      <Provider store={elementStore}>
-        <Animal key={animal[0].id} dragon={animal[0]} />
-      </Provider>,
-    );
-
-    // ASSERT
-    expect(container).toMatchSnapshot();
+describe('Testing Animal', () => {
+  it('Render the Details page, should match to the snapshot', () => {
+    const list = renderer
+      .create(
+        <Provider store={store}>
+          <Router>
+            <Animal />
+          </Router>
+        </Provider>,
+      )
+      .toJSON();
+    expect(list).toMatchSnapshot();
   });
 });
