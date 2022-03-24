bfdae2f8e547178b187612e693defe844f0672e6
david-lafontant <lafontant.david@gmail.com>
2022-03-23 21:51:56 -0600

    Update Header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMIcrophone } from '@fortawesome/free-solid-svg-icons';
+import HeaderCSS from './header.module.css';
+
 function Header() {
   return (
-    <header>
+    <Container className={HeaderCSS.background}>
+      <Row>
+      <FontAwesomeIcon icon="fa-solid fa-microphone" />
+      </Row>
+      <Row className={HeaderCSS.headerRow}>
+        <Col className={HeaderCSS.banner} sm={6} lg={6} />
+        <Col sm={6} lg={6}>
+          <h1>
+            Zoo
+          </h1>
+          <h2>
+            Animals
+          </h2>
+        </Col>
 
-      <h1>Header</h1>
-
-    </header>
+      </Row>
+    </Container>
 
   );
 }
