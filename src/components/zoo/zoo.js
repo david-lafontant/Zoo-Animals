a3e5a10e671299e372e1b709e1690e8076f5100b
david-lafontant <lafontant.david@gmail.com>
2022-03-23 21:52:10 -0600

    Update Zoo component

--- src/components/zoo/zoo.js
+++ src/components/zoo/zoo.js
@@ -1,6 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import Animal from '../animal/animal';
+import ZooCSS from './zoo.module.css';
 
 function Zoo() {
   const zooAnimals = useSelector((state) => state.zoo);
@@ -8,40 +12,41 @@ function Zoo() {
   const [search, setSearch] = useState('');
 
   return (
-    <div>
+    <Container>
       <input
         type="text"
         onChange={({ target }) => {
           setSearch(target.value);
         }}
         placeholder="Search by type"
-        className="input"
+        className="myInput input form-control row"
       />
-
-      {zooAnimals
-        .filter((item) => item.animal_type.toLowerCase().includes(search.toLowerCase()))
-        .map((item) => (
-          <Animal
-            key={item.id}
-            id={item.id}
-            type={item.animal_type}
-            name={item.name}
-            image={item.image_link}
-            latin_name={item.latin_name}
-            active_time={item.active_time}
-            length_min={item.length_min}
-            length_max={item.length_max}
-            weight_min={item.weight_min}
-            weight_max={item.weight_max}
-            lifespan={item.lifespan}
-            habitat={item.habitat}
-            diet={item.diet}
-            geo_range={item.geo_range}
-            detail={item.detail}
-            item_detail={item.item_detail}
-          />
-        ))}
-    </div>
+      <Row className={ZooCSS.zooRow}>
+        {zooAnimals
+          .filter((item) => item.animal_type.toLowerCase().includes(search.toLowerCase()))
+          .map((item) => (
+            <Animal
+              key={item.id}
+              id={item.id}
+              type={item.animal_type}
+              name={item.name}
+              image={item.image_link}
+              latin_name={item.latin_name}
+              active_time={item.active_time}
+              length_min={item.length_min}
+              length_max={item.length_max}
+              weight_min={item.weight_min}
+              weight_max={item.weight_max}
+              lifespan={item.lifespan}
+              habitat={item.habitat}
+              diet={item.diet}
+              geo_range={item.geo_range}
+              detail={item.detail}
+              item_detail={item.item_detail}
+            />
+          ))}
+      </Row>
+    </Container>
   );
 }
 
