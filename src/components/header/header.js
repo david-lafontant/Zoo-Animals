72e175a267817536ea16d10da6863fb2b07ed944
david-lafontant <lafontant.david@gmail.com>
2022-03-24 10:39:22 -0600

    Style Header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,31 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faMIcrophone } from '@fortawesome/free-solid-svg-icons';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
 import HeaderCSS from './header.module.css';
+import banner from '../../assets/banner.jpg';
+import micro from '../../assets/microphoneSvgrepo.svg';
+import gear from '../../assets/settingsGearSvgrepo.svg';
 
 function Header() {
   return (
-    <Container className={HeaderCSS.background}>
-      <Row>
-      <FontAwesomeIcon icon="fa-solid fa-microphone" />
+    <Row className={HeaderCSS.background}>
+      <Row className={HeaderCSS.iconContainer}>
+        <Image src={micro} className={HeaderCSS.imgIcon} alt="Microphone" />
+        <Image src={gear} className={HeaderCSS.imgIcon} alt="Setting gear" />
       </Row>
       <Row className={HeaderCSS.headerRow}>
-        <Col className={HeaderCSS.banner} sm={6} lg={6} />
-        <Col sm={6} lg={6}>
-          <h1>
-            Zoo
-          </h1>
-          <h2>
-            Animals
-          </h2>
+        <Col className={HeaderCSS.banner} sm={6} md={6} lg={6}>
+          <Card.Img src={banner} style={{ width: '50%' }} className="logo" alt="Panthera" />
         </Col>
-
+        <Col className={HeaderCSS.titleContent} sm={6} md={6} lg={6}>
+          <h1>Zoo</h1>
+          <h2>Animals</h2>
+        </Col>
       </Row>
-    </Container>
-
+    </Row>
   );
 }
 
